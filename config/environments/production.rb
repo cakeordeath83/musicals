0f8622650e25e98a06c6a413c063e578a76d2940
Clair Griffiths <clair_griffiths@hotmail.com>
2015-08-29 17:14:21 +0000

    Trying to fix images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,9 +7,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :bucket => ENV['clairsnewbucket'],
+    :access_key_id => ENV['AKIAJAZTAGCHDQNF2B4Q'],
+    :secret_access_key => ENV['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']
   }
 }
   # Eager load code on boot. This eager loads most of Rails and
